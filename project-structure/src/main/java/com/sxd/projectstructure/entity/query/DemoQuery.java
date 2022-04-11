6193b00bd988c94aab051c73032dec8e93bbe23d
LiJianxin <844485538@qq.com>
2022-04-11 16:36:41 +0800

    fix： 优化目录名称

---
+++ project-structure/src/main/java/com/sxd/projectstructure/entity/query/DemoQuery.java
@@ -0,0 +1,21 @@
+package com.sxd.projectstructure.entity.query;
+
+/**
+ * @Author 李健新
+ * @Date 2022/3/31
+ * @Description
+ *
+ *      查询参数实体类
+ */
+public class DemoQuery {
+
+    private Integer param;
+
+    public Integer getParam() {
+        return param;
+    }
+
+    public void setParam(Integer param) {
+        this.param = param;
+    }
+}
