189d0da53e102bbacfaf80be1462e1f6c3f1bb66
LiJianxin <844485538@qq.com>
2022-04-11 16:47:39 +0800

    fix： 优化

--- project-structure/src/main/java/com/sxd/projectstructure/ProjectStructureApplication.java
+++ project-structure/src/main/java/com/sxd/projectstructure/ProjectStructureApplication.java
@@ -4,6 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 李健新
+ *
+ *    框架模板示例工程
+ */
 @MapperScan(basePackages = "com.sxd.projectstructure.mapper")
 @SpringBootApplication
 public class ProjectStructureApplication {
